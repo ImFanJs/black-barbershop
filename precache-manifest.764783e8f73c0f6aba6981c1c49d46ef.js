09d5f21da74455ecf7a06c61c2916eeed42ccea4
imfanjs <imfanjs@gmail.com>
2020-04-09 14:50:25 -0600

    Updates

---
+++ precache-manifest.764783e8f73c0f6aba6981c1c49d46ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6f25b3b0845df2015b782ba96554c0",
+    "url": "/ImFanJs/black-barbershop/index.html"
+  },
+  {
+    "revision": "f2f75f676d01c687dea1",
+    "url": "/ImFanJs/black-barbershop/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "562c80ed9958fdfaec09",
+    "url": "/ImFanJs/black-barbershop/static/css/main.dc090a44.chunk.css"
+  },
+  {
+    "revision": "f2f75f676d01c687dea1",
+    "url": "/ImFanJs/black-barbershop/static/js/2.2d1f5959.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ImFanJs/black-barbershop/static/js/2.2d1f5959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "562c80ed9958fdfaec09",
+    "url": "/ImFanJs/black-barbershop/static/js/main.ba563360.chunk.js"
+  },
+  {
+    "revision": "bf0031271335da8e0c7a",
+    "url": "/ImFanJs/black-barbershop/static/js/runtime-main.9a3c2b5f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ImFanJs/black-barbershop/static/media/logo.5d5d9eef.svg"
+  }
+]);
